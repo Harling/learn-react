06eeee1b6a68529a2473f12f2f0bcf293a9c2c76
liufei <liu.fei@immomo.com>
2017-03-09 17:10:05 +0800

    init ...

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  devtool: 'eval-source-map',
+
+  entry:  __dirname + "/app/main.js",
+  output: {
+    path: __dirname + "/public",
+    filename: "bundle.js"
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.json$/,
+        loader: "json-loader"
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',//在webpack的module部分的loaders里进行配置即可
+        query: {
+          presets: ['es2015','react']
+        }
+      }
+    ]
+  },
+
+  devServer: {
+    contentBase: "./public",
+    // colors: true,
+    historyApiFallback: true,
+    inline: true
+  }
+}
