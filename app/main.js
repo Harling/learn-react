5be89e707c9c427415a321b395f431407074dd76
liufei <liu.fei@immomo.com>
2017-03-10 16:34:04 +0800

    ComposingComponents

--- app/main.js
+++ app/main.js
@@ -2,22 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 /*
-Previously, we only encountered React elements that represent DOM tags:
-// const element = <div />;
-
-However, elements can also represent user-defined components:
-//const element = <Welcome name="Sara" />;
- */
-
+Components can refer to other components in their output.  
+*/
 class Welcome extends React.Component {
   render() {
     return <h1>Hello, {this.props.name}</h1>;
   }
 }
 
-const element = <Welcome name="Harling" />;
+class WelcomBoard extends React.Component {
+  render() {
+    return (
+        <div>
+          <Welcome name="Harling" />
+          <Welcome name="Liufei" />
+        </div>
+    )
+  }
+}
 
 ReactDOM.render(
-  element,
+  <WelcomBoard />,
   document.getElementById('root')
 );
