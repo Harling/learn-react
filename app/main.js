027bf2ea00e7e27e8760eca397815d288f01de86
liufei <liu.fei@immomo.com>
2017-03-10 15:07:14 +0800

    RenderingAnElementIntoTheDOM

--- app/main.js
+++ app/main.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const element = <h1>Hello, world!</h1>;
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  element,
   document.getElementById('root')
+);
+
+ReactDOM.render(
+  element, // React element
+  document.getElementById('root') // DOM node
 );
