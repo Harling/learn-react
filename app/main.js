634db6a47f50be753d029d57eb05398bba163f8d
liufei <liu.fei@immomo.com>
2017-03-10 17:25:47 +0800

    ExtractingComponents

--- app/main.js
+++ app/main.js
@@ -2,26 +2,90 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 /*
-Components can refer to other components in their output.  
+1. generic name
+2. more case
 */
-class Welcome extends React.Component {
+
+class CommentA extends React.Component {
   render() {
-    return <h1>Hello, {this.props.name}</h1>;
+    const props = this.props;
+    return (
+      <div className="Comment">
+        <div className="UserInfo">
+          <img className="Avatar"
+            src={props.author.avatarUrl}
+            alt={props.author.name}
+          />
+          <div className="UserInfo-name">
+            {props.author.name}
+          </div>
+        </div>
+        <div className="Comment-text">
+          {props.text}
+        </div>
+        <div className="Comment-data">
+          {props.data}
+        </div>
+      </div>
+    );
   }
 }
 
-class WelcomBoard extends React.Component {
+class Avatar extends React.Component {
   render() {
+    const props = this.props;
     return (
-        <div>
-          <Welcome name="Harling" />
-          <Welcome name="Liufei" />
+      <img className="Avatar"
+        src={props.user.avatarUrl}
+        alt={props.user.name}
+      />
+    );
+  }
+}
+
+class UserInfo extends React.Component {
+  render() {
+    const props = this.props;
+    return (
+      <div className="UserInfo">
+        <Avatar user={props.user} />
+        <div className="UserInfo-name">
+          {props.user.name}
         </div>
-    )
+      </div>
+    );
+  }
+}
+
+class CommentB extends React.Component {
+  render () {
+    const props = this.props;
+    return (
+      <div className="Comment">
+        <UserInfo user={props.author} />
+        <div className="Comment-text">
+          {props.text}
+        </div>
+        <div className="Comment-data">
+          {props.data}
+        </div>
+      </div>
+    );
   }
 }
 
 ReactDOM.render(
-  <WelcomBoard />,
+  <div>
+  <CommentA
+    author={{name: "Harling", avatarUrl: "http://localhost/avatarUrl/harling/" }}
+    data="Comment-data"
+    text="Comment-text"
+  />
+  <CommentB
+    author={{name: "Harling", avatarUrl: "http://localhost/avatarUrl/harling/" }}
+    data="Comment-data"
+    text="Comment-text"
+  />
+  </div>,
   document.getElementById('root')
 );
