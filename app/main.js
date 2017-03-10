2bcd399c04a22e2942fba961b4e27a19295b711b
liufei <liu.fei@immomo.com>
2017-03-10 18:48:20 +0800

    4StateandLifecycle

--- app/main.js
+++ app/main.js
@@ -1,7 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+/*
+lifecycle hooks
+mounting, unmounting
+constructor -> render -> componentDidMount -> componentWillUnmount
+*/
+
+class Clock extends React.Component {
+  constructor(props) { // init state
+    super(props);
+    this.state = {date: new Date()};
+  }
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <Clock />,
   document.getElementById('root')
 );
---
+++ readme.txt
@@ -0,0 +1,4 @@
+State is similar to props, but it is private and fully controlled by the component.
+
+We mentioned before that components defined as classes have some additional features.
+Local state is exactly that: a feature available only to classes.
