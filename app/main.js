afa4745cbce38d14726bb876e05b219b28bfdea7
liufei <liu.fei@immomo.com>
2017-03-10 12:25:40 +0800

    Hello World

--- app/main.js
+++ app/main.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {render} from 'react-dom';
-import Greeter from './Greeter';
-import ShoppingList from './shoppingList';
+import ReactDOM from 'react-dom';
 
-render(<div><Greeter /> <ShoppingList name="Mark"/></div>, document.getElementById('root'));
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
