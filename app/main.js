ca5ff6594a7843628d2d4786df98c0bb90cc8749
liufei <liu.fei@immomo.com>
2017-03-10 14:53:15 +0800

    JSXRepresentsObjects

--- app/main.js
+++ app/main.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+/*
+Babel compiles JSX down to React.createElement() calls.
+*/
 
+/*
 ReactDOM.render(
   <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
+*/
+
+const element = React.createElement(
+  'h1', // tag name
+  {},   // 属性
+  'Hello, world!' //children
+);
+
+ReactDOM.render(
+  element,
   document.getElementById('root')
 );
