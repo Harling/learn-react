26a4d5afbd72a22fff26c6ed5635df942e655c5b
liufei <liu.fei@immomo.com>
2017-03-10 15:37:08 +0800

    UpdatingTheRenderedElement

--- app/main.js
+++ app/main.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+/*
+React elements are immutable.
+create a new element.
+*/
 
-const element = <h1>Hello, world!</h1>;
-ReactDOM.render(
-  element,
-  document.getElementById('root')
-);
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(
+    element,
+    document.getElementById('root') // 兄弟节点会被覆盖, 所以节此点下总是只有本次render结果.g
+  );
+}
 
-ReactDOM.render(
-  element, // React element
-  document.getElementById('root') // DOM node
-);
+setInterval(tick, 1000);
