40e8b8c84a32bd45d2b2837fa6bc8ad477ede09e
liufei <liu.fei@immomo.com>
2017-03-10 16:10:26 +0800

    FunctionalAndClassComponents

--- app/main.js
+++ app/main.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+/* Functional */
+function WelcomeF(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+/* Class */
+class WelcomeC extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
+}
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <div>
+    <WelcomeF name="Functional Component" />
+    <WelcomeC name="Class Component" />
+  </div>,
   document.getElementById('root')
 );
