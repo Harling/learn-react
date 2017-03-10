fda39f097fa842070f183480f8167a6c6d394146
liufei <liu.fei@immomo.com>
2017-03-10 14:28:48 +0800

    SXIsAnExpressionToo

--- app/main.js
+++ app/main.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+/* 
+After compilation, JSX expressions become regular JavaScript objects.
+This means that you can use JSX inside of if statements and for loops, 
+assign it to variables, accept it as arguments, and return it from functions:
+*/
+
 function formatName(user) {
   return user.firstName + ' ' + user.lastName;
 }
@@ -10,14 +16,14 @@ const user = {
   lastName: 'Perez'
 };
 
-// const element = <h1>Hello, world!</h1>; jsx
-const element = (
-  <h1>
-    Hello, {formatName(user)}!
-  </h1>
-);
+function getGreeting(user) {
+  if (user) {
+    return <h1>Hello, {formatName(user)}!</h1>;
+  }
+  return <h1>Hello, Stranger.</h1>;
+}
 
 ReactDOM.render(
-  element,
+  getGreeting(user),
   document.getElementById('root')
 );
