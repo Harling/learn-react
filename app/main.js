0786c4e6f1a9346721325866ebc520f29c68b7a4
liufei <liu.fei@immomo.com>
2017-03-13 11:49:42 +0800

    InlineIf-ElseWithConditionalOperator

--- app/main.js
+++ app/main.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <div>
+    Harling is <b>{true ? 'currently' : 'not'}</b> logged in.
+    <hr />
+    Liufei is <b>{false ? 'currently' : 'not'}</b> logged in.
+  </div>,
   document.getElementById('root')
 );
