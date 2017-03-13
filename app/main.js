453417954d4bfb9c117c9c68ef6ec3a7e4bab71a
liufei <liu.fei@immomo.com>
2017-03-13 11:15:06 +0800

    6ConditionalRendering

--- app/main.js
+++ app/main.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+function UserGreeting(props) {
+  return <h1>Welcome back!</h1>;
+}
+
+function GuestGreeting(props) {
+  return <h1>Please sign up.</h1>;
+}
+
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn) {
+    return <UserGreeting />;
+  }
+  return <GuestGreeting />;
+}
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <div>
+		<Greeting isLoggedIn={false} />
+		<hr />
+		<Greeting isLoggedIn={true} />
+  </div>,
   document.getElementById('root')
 );
