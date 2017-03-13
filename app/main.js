2f7ec358111be8c296442b682a3f24d7df7bbf8b
liufei <liu.fei@immomo.com>
2017-03-13 17:44:30 +0800

    textarea.select.multiple

--- app/main.js
+++ app/main.js
@@ -1,46 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-/* DomForm uncontroled Form*/
-class DomForm extends React.Component {
-  render() {
-    return (
-     <form>
-       <label>
-       Name:
-       <input type="text" name="name" />
-       </label>
-       <input type="submit" value="Submit" />
-     </form>
-    )
-  }
-}
-
-/* controled form */
 class RtForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
-
+    this.state = {
+      value_textarea: 'Please write an essay about your favorite DOM element.',
+      value_select: 'coconut'
+    };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
-
-  handleChange(event) {
-    this.setState({value: event.target.value}); //!!
-  }
-
+	handleChange(event) { //!! handling multiple input
+		const target = event.target;
+    console.log("target.type", target.type) // input textarea radio checkbox select-one select-multi ...
+		const value = target.type === 'checkbox' ? target.checked : target.value;
+		const name = target.name;
+		this.setState({
+			[name]: value
+		});
+	}
   handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
+    alert('An essay was submitted: ' + this.state.value);
     event.preventDefault();
   }
-
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
+				{/* textarea */}
         <label>
           Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} /> //!!
+          <textarea name="value_textarea" value={this.state.value_textarea} onChange={this.handleChange} /> //!! name
+        </label>
+				{/* select */}
+        <label>
+          Pick your favorite La Croix flavor:
+          <select name="value_select" value={this.state.value_select} onChange={this.handleChange}> //!! name
+            <option value="grapefruit">Grapefruit</option>
+            <option value="lime">Lime</option>
+            <option value="coconut">Coconut</option>
+            <option value="mango">Mango</option>
+          </select>
         </label>
         <input type="submit" value="Submit" />
       </form>
@@ -49,9 +49,6 @@ class RtForm extends React.Component {
 }
 
 ReactDOM.render(
-  <div>
-		<DomForm />
-		<RtForm />
-  </div>,
+  <RtForm />,
   document.getElementById('root')
 );
